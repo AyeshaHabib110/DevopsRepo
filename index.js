91442d98296ba8fb7dfa1096656d8d28251ee69c
root <root@ip-172-31-46-140.eu-north-1.compute.internal>
2024-10-23 10:02:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname)));
+
+// Serve index.html for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Define the port
+const PORT = process.env.PORT || 3000;
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
